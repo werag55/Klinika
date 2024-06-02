cc2d69050ccc21a13a7102bd8181b3fb96afba49
Weronika Gozdera <weronika.gozdera@gmail.com>
2024-06-02 23:14:40 +0200

    fixed CreateClienCommand (was not updated to use DTO)

--- src/Klinika.Application/Clients/CreateClient/CreateClientCommandHandler.cs
+++ src/Klinika.Application/Clients/CreateClient/CreateClientCommandHandler.cs
@@ -1,18 +1,23 @@
-﻿using Klinika.Domain.Models;
+﻿using AutoMapper;
+using Klinika.Application.Clients.ClientsDTO;
+using Klinika.Domain.Models;
 using Klinika.Domain.Repositories;
 using MediatR;
 
 namespace Klinika.Application.Clients.CreateClient;
 
-public class CreateClientCommandHandler(IClientRepository ClientRepository, IUnitOfWork unitOfWork) 
-    : IRequestHandler<CreateClientCommand, Client>
+public class CreateClientCommandHandler(IClientRepository ClientRepository, IUnitOfWork unitOfWork, IMapper Mapper) 
+    : IRequestHandler<CreateClientCommand, UpsertClientDTO>
 {
     private readonly IClientRepository _ClientRepository = ClientRepository;
     private readonly IUnitOfWork _unitOfWork = unitOfWork;
+    private readonly IMapper _Mapper = Mapper;
 
-    public async Task<Client> Handle(CreateClientCommand request, CancellationToken cancellationToken)
+    public async Task<UpsertClientDTO> Handle(CreateClientCommand request, CancellationToken cancellationToken)
     {
-        _ClientRepository.Add(request.Client);
+        Client client = _Mapper.Map<Client>(request.Client);
+
+        _ClientRepository.Add(client);
         await _unitOfWork.SaveChangesAsync();
         return request.Client;
     }
